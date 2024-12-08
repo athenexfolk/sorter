52cebbc066b8a942c249b52fa13c3efeacf2549e
athenexfolk <folkadpocket99@gmail.com>
2024-12-08 22:34:41 +0700

    fixed document is not defined

--- src/app/shared/directives/mathtex.directive.ts
+++ src/app/shared/directives/mathtex.directive.ts
@@ -1,4 +1,11 @@
-import { Directive, ElementRef, Input } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import {
+  Directive,
+  ElementRef,
+  Inject,
+  Input,
+  PLATFORM_ID,
+} from '@angular/core';
 import katex from 'katex';
 
 @Directive({
@@ -7,10 +14,13 @@ import katex from 'katex';
 export class MathtexDirective {
   @Input('mathtex') expression!: string;
 
-  constructor(private el: ElementRef) {}
+  constructor(
+    private el: ElementRef,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {}
 
   ngOnChanges(): void {
-    if (this.expression) {
+    if (this.expression && isPlatformBrowser(this.platformId)) {
       katex.render(this.expression, this.el.nativeElement, {
         throwOnError: false,
         displayMode: true,
