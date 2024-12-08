7e1c7d2f827e9b5a24a224dd70827902035a76b9
athenexfolk <folkadpocket99@gmail.com>
2024-12-08 21:06:47 +0700

    added landing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,19 @@
 import { Routes } from '@angular/router';
 import { BubbleSortDetailComponent } from './features/bubble-sort/pages/bubble-sort-detail/bubble-sort-detail.component';
 import { SelectionSortDetailComponent } from './features/selection-sort/pages/selection-sort-detail/selection-sort-detail.component';
+import { LandingComponent } from './features/landing/pages/landing/landing.component';
 
 export const routes: Routes = [
   {
     path: '',
+    component: LandingComponent,
+  },
+  {
+    path: 'bubble-sort',
     component: BubbleSortDetailComponent,
   },
   {
     path: 'selection-sort',
-    component: SelectionSortDetailComponent
-  }
+    component: SelectionSortDetailComponent,
+  },
 ];
